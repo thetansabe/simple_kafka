9046918fa20148556afa1df0f0d006e7bd6d026c
Nguyen Ton Dien <ntdien@Nguyens-MacBook-Pro.local>
2026-08-03 14:44:27 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -14,16 +14,39 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// TODO: Uncomment the code below to pass the first stage
-
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	for {
+		conn, err := l.Accept()
+
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 1024) // Create a buffer to hold incoming data, 1024 bytes because that's the maximum size of a Kafka message
+
+	_, err := conn.Read(buf) // Load conn data into the buffer
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		fmt.Println("Error reading data: ", err.Error())
+		return
+	}
+
+	_, err = conn.Write([]byte{0, 0, 0, 1, 0, 0, 0, 7}) // 8 bytes total, 0001 (any value) is for message_size and 0007 is for correlation_id (must be 7)
+
+	if err != nil {
+		fmt.Println("Error writing data: ", err.Error())
+		return
 	}
 }
